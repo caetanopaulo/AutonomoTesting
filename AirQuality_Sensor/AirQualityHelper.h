2f938261ccc58fb5b56adf56a966399c2df9808b
caetanopaulo <caetano.paulo@gmail.com>
2016-11-03 09:42:32 +0100

    Added a AirQualityHelper class

---
+++ AirQuality_Sensor/AirQualityHelper.h
@@ -0,0 +1,12 @@
+#ifndef __AIRQUALITYHELPER_H__
+#define __AIRQUALITYHELPER_H__
+
+#include"Arduino.h"
+#include"AirQuality.h"
+
+class AirQualityHelper
+{
+public:
+    void configureTC3a();
+ };   
+#endif
